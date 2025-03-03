a10eac24d4fdd38846eab3d0a8e4167405731830
Ollie Moss <olliemoss321@gmail.com>
2025-03-04 03:11:54 +1300

    Finished weather route and added server

--- app.js
+++ app.js
@@ -21,15 +21,49 @@ app.get("/", (req, res) => {
     res.render("index", { weather: null, error: null });
 });
 
-app.post("/weather", (req, res) => {
+app.post("/weather", async (req, res) => {
     console.log("POST req recieved at /weather");
     const city = req.body.city;
     console.log(`User entered city: ${city}`);
 
     if (!city) {
         console.log("No City entered");
-        res.render("index", { weather: null, error: "Please enter a city name" });
+        res.render("index", {
+            weather: null,
+            error: "Please enter a city name",
+        });
         return;
     }
 
+    try {
+        console.log(`Fetching weather data for city ${city}`);
+        const response = await axios.get(
+            `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`,
+        );
+        console.log("Weather data fetched succesfully");
+        const weatherData = response.data;
+
+        const weather = {
+            city: weatherData.name,
+            temperature: weatherData.main.temperature,
+            description: weatherData.weather[0].description,
+            icon: `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
+        };
+
+        console.log(
+            `Weather for ${weather.city}: ${weather.temperature}, ${weather.description}`,
+        );
+
+        res.render("index", { weather, error: null });
+    } catch (error) {
+        console.error(`Failed to fetch weather data: ${error.message}`);
+        res.render("index", {
+            weather: null,
+            error: `Failed to fetch weather data`,
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
